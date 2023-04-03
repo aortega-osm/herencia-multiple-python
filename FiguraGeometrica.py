5170608d83b532d80dcf741b7094c0452ea02cf1
Alejandro ortega <aortega@osm-soft.io>
2023-04-03 17:57:18 -0300

    Se realizo el ejercicio

---
+++ FiguraGeometrica.py
@@ -0,0 +1,28 @@
+# class FiguraGeometrica:
+#     def __init__(self,ancho,alto):
+#         self.ancho=ancho
+#         self.alto=alto
+
+
+class FiguraGeometrica:
+    def __init__(self,alto,ancho):
+        self._alto=alto
+        self._ancho=ancho
+
+    def __str__(self):
+        return f"El alto es: {self._alto} y el ancho es: {self._ancho}"
+
+    @property
+    def alto(self):
+        return self._alto
+    @alto.setter
+    def alto(self,alto):
+        self._alto=alto
+
+    @property
+    def ancho(self):
+        return self._ancho
+
+    @ancho.setter
+    def ancho(self,ancho):
+        self._ancho=ancho
